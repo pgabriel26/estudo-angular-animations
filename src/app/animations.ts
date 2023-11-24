2c8e0d0c931cfd3290424b88cee0bbc4eb4f29dc
pgabriel <pgabriel.26@gmail.com>
2023-11-24 16:35:25 -0300

    Desafio de animar botao de tarefaz resolvida concluido

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -32,3 +32,11 @@ export const showStateTrigger = trigger('showState', [
         }))
     ]),
 ])
+
+export const clickConfirmationTrigger = trigger('clickConfirmation', [
+    transition('* => checked', [
+        animate('400ms ease-in', style({
+            transform: 'scale(0.22)'
+        }))
+    ])
+])
--- src/app/lista-tarefas/lista-tarefas.component.ts
+++ src/app/lista-tarefas/lista-tarefas.component.ts
@@ -4,13 +4,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
-import { highlightedStateTrigger, showStateTrigger } from '../animations';
+import { clickConfirmationTrigger, highlightedStateTrigger, showStateTrigger } from '../animations';
 
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
   styleUrls: ['./lista-tarefas.component.css'],
-  animations: [highlightedStateTrigger, showStateTrigger]
+  animations: [highlightedStateTrigger, showStateTrigger, clickConfirmationTrigger]
 })
 export class ListaTarefasComponent implements OnInit {
   listaTarefas: Tarefa[] = [];
@@ -18,6 +18,7 @@ export class ListaTarefasComponent implements OnInit {
   categoria: string = '';
   validado: boolean = false;
   indexTarefa = -1;
+  idTarefa: number = 0
 
   formulario: FormGroup = this.fomBuilder.group({
     id: [0],
@@ -110,6 +111,7 @@ export class ListaTarefasComponent implements OnInit {
   }
 
   finalizarTarefa(id: number) {
+    this.idTarefa = id;
     this.service.buscarPorId(id!).subscribe((tarefa) => {
       this.service.atualizarStatusTarefa(tarefa).subscribe(() => {
         this.listarAposCheck();
